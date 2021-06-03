a6117420ab7662b336e8a05eb3ad73258633d4d7
Frank V. Castellucci <frankiecast@gmail.com>
2021-06-03 05:03:11 -0400

    Add # Safety comment and unit test block

--- main/src/main.rs
+++ main/src/main.rs
@@ -23,51 +23,69 @@ pub struct lk_msg {
     pub m_data: [libc::c_char; 984],
 }
 
+/// # Safety
+/// Chyrsalisp exit... this will stop the boat
 pub unsafe extern "C" fn exit(status: i32) {
     ::std::process::exit(status);
 }
 
 
+/// # Safety
+/// Chyrsalisp pii_stat
 pub unsafe extern "C" fn pii_stat(si: &pii_stat_info) -> libc::c_int {
     println!("pii_start");
 
     1
 }
 
+/// # Safety
+/// Chyrsalisp pii_open
 pub unsafe extern "C" fn pii_open(mut path: *const libc::c_char,
                                   mut mode: libc::c_int) -> libc::c_int {
     let mut fd: libc::c_int = 0;
+    -(1 as libc::c_int)
+}
+
+/// # Safety
+/// Chyrsalisp pii_close
+pub unsafe extern "C" fn pii_close(handle: libc::c_int) -> libc::c_int {
     return -(1 as libc::c_int);
 }
 
-pub unsafe extern "C" fn pii_close(handle: libc::c_int) -> libc::c_int {
-    return -(1 as libc::c_int);   
-}
-
+/// # Safety
+/// Chyrsalisp pii_unlink (delete)
 pub unsafe extern "C" fn pii_unlink(mut path: *const libc::c_char)
  -> libc::c_int {
-    return -(1 as libc::c_int);   
+    return -(1 as libc::c_int);
 }
 
+/// # Safety
+/// Chyrsalisp pii_read
 pub unsafe extern "C" fn pii_read(mut fd: libc::c_int,
                                   mut addr: *mut libc::c_void,
                                   mut len: libc::c_int) -> libc::c_int {
-    return -(1 as libc::c_int);   
+    return -(1 as libc::c_int);
 }
 
+/// # Safety
+/// Chyrsalisp pii_write
 pub unsafe extern "C" fn pii_write(mut fd: libc::c_int,
                                   mut addr: *mut libc::c_void,
                                   mut len: libc::c_int) -> libc::c_int {
-    return -(1 as libc::c_int);   
+    return -(1 as libc::c_int);
 }
 
+/// # Safety
+/// Chyrsalisp pii_mmap for memory map allication
 #[no_mangle]
 pub unsafe extern "C" fn pii_mmap(mut len: libc::c_int, mut fd: libc::c_int,
                                   mut mode: libc::c_int)
- -> *mut libc::c_void {    
+ -> *mut libc::c_void {
     return -(1 as libc::c_int) as *mut libc::c_void;
 }
 
+/// # Safety
+/// Chyrsalisp pii_munmap for memory map deallocation
 #[no_mangle]
 pub unsafe extern "C" fn pii_munmap(mut addr: *mut libc::c_void,
                                     mut len: libc::c_int,
@@ -75,6 +93,8 @@ pub unsafe extern "C" fn pii_munmap(mut addr: *mut libc::c_void,
     return -(1 as libc::c_int);
 }
 
+/// # Safety
+/// Chyrsalisp pii_mprotect for memory map
 #[no_mangle]
 pub unsafe extern "C" fn pii_mprotect(mut addr: *mut libc::c_void,
                                       mut len: libc::c_int,
@@ -82,10 +102,14 @@ pub unsafe extern "C" fn pii_mprotect(mut addr: *mut libc::c_void,
      1
 }
 
+/// # Safety
+/// Chyrsalisp pii_gettime
 pub unsafe extern "C" fn pii_gettime() -> libc::c_int{
     0
 }
 
+/// # Safety
+/// Chyrsalisp pii_open_shared
 #[no_mangle]
 pub unsafe extern "C" fn pii_open_shared(mut path: *const libc::c_char,
                                          mut len: libc::c_int)
@@ -93,6 +117,8 @@ pub unsafe extern "C" fn pii_open_shared(mut path: *const libc::c_char,
     1
 }
 
+/// # Safety
+/// Chyrsalisp pii_close shared
 #[no_mangle]
 pub unsafe extern "C" fn pii_close_shared(mut path: *const libc::c_char,
                                           mut hndl: libc::c_int)
@@ -100,6 +126,8 @@ pub unsafe extern "C" fn pii_close_shared(mut path: *const libc::c_char,
     1
 }
 
+/// # Safety
+/// Chyrsalisp pii_flush_icache
 #[no_mangle]
 pub unsafe extern "C" fn pii_flush_icache(mut addr: *mut libc::c_void,
                                           mut len: libc::c_int)
@@ -107,6 +135,8 @@ pub unsafe extern "C" fn pii_flush_icache(mut addr: *mut libc::c_void,
     return addr;
 }
 
+/// # Safety
+/// Chyrsalisp pii_dirlist
 #[no_mangle]
 pub unsafe extern "C" fn pii_dirlist(mut path: *const libc::c_char,
                                      mut buf: *mut libc::c_char,
@@ -115,6 +145,8 @@ pub unsafe extern "C" fn pii_dirlist(mut path: *const libc::c_char,
     return 2 as libc::c_int;
 }
 
+/// # Safety
+/// Chyrsalisp pii_remove
 #[no_mangle]
 pub unsafe extern "C" fn pii_remove(mut fqname: *const libc::c_char)
  -> libc::c_int {
@@ -122,17 +154,23 @@ pub unsafe extern "C" fn pii_remove(mut fqname: *const libc::c_char)
     return res;
 }
 
+/// # Safety
+/// Chyrsalisp pii_seek
 #[no_mangle]
 pub unsafe extern "C" fn pii_seek(mut fd: libc::c_int, mut pos: libc::c_int,
                                   mut offset: libc::c_uchar) -> libc::c_int {
     1
 }
 
+/// # Safety
+/// Chyrsalisp pii_random
 pub unsafe extern "C" fn pii_random() -> libc::c_int{
     println!("pii_random");
     1
 }
 
+/// # Safety
+/// Chyrsalisp pii_sleep
 pub unsafe extern "C" fn pii_sleep(delay: u64) {
     println!("pii_sleep");
 }
@@ -143,18 +181,24 @@ pub static mut lk_data_size: libc::c_int = 984 as libc::c_int;
 pub static mut lk_page_size: libc::c_int = 4096 as libc::c_int;
 #[no_mangle]
 pub static mut usb_monitor: libc::c_int = 0;
+/// # Safety
+/// Chyrsalisp pii_usb_start
 #[no_mangle]
 pub unsafe extern "C" fn pii_usb_start(mut buffer: *mut lk_msg)
  -> libc::c_int {
     return 0 as libc::c_int;
 }
 
+/// # Safety
+/// Chyrsalisp pii_usb_stop
 #[no_mangle]
 pub unsafe extern "C" fn pii_usb_stop(mut buffer: *mut lk_msg)
  -> libc::c_int {
     return 0 as libc::c_int;
 }
 
+/// # Safety
+/// Chyrsalisp pii_usb_running
 #[no_mangle]
 pub unsafe extern "C" fn pii_usb_running(mut buffer: *mut lk_msg)
  -> libc::c_int {
@@ -173,11 +217,17 @@ fn main() {
 
 
     // image file is now mapped
-    unsafe {
+    // unsafe {
+    // }
+}
 
-
-
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_gettime() {
+        unsafe {
+            println!("{}", pii_gettime());
+        }
     }
-
-
 }
